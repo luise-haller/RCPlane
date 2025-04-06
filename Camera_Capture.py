cfaddd0ed35dd56382ecf618cebc77f42549fd12
Luise Haller <126217517+luise-haller@users.noreply.github.com>
2025-04-06 10:21:16 -0700

    Changed to utc time format, writing vid metadata to manifest file (makes ML grid coordination easier)

--- Camera_Capture.py
+++ Camera_Capture.py
@@ -1,7 +1,8 @@
 import os
-from datetime import datetime
+from datetime import datetime, timezone
 import time
 import subprocess
+from gps_utils import find_closest_gps
 
 output_dir = os.path.expanduser("~/Videos")
 os.makedirs(output_dir, exist_ok="True")
@@ -11,7 +12,8 @@ duration_of_vid = 300 # 5mins
 # Records 5min videos continously and saves them in the Videos dir
 def record_vid():
     while True:
-        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        start_time = datetime.now(timezone.utc)
+        timestamp = start_time.strftime("%Y-%m-%d_%H-%M-%S")
         fname = f"{timestamp}.mp4"
         fpath = os.path.join(output_dir, fname)
 
@@ -24,6 +26,23 @@ def record_vid():
 
         print("Filepath saved to is: " + fpath)
         subprocess.run(vid_record_command, check=True)
+
+        end_time = datetime.now(timezone.utc)
+        log_video_metadata(fname, start_time, end_time)
+
+def log_video_metadata(fname, start, end):
+    manifest_path = os.path.join(output_dir, "video_manifest.csv")
+    header = not os.path.exists(manifest_path)
+
+    with open(manifest_path, 'a') as f:
+        wr = csv.writer(f)
+        if header:
+            wr.writerow(['filename','start_utc','end_utc','start_lat','start_lon'])
+        
+        # getting the gps that lies closests to the start + end times
+        gps_coord = find_closest_gps(start)
+        wr.writerow([fname, start.isoformat(), end.isoformat(), 
+                       gps_coord['lat'], gps_coord['lon']])
         
 if __name__ == "__main__":
     try:
